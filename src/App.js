0e9f208007aebac4d20fc5c827d10b4750c6a4f4
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-05-22 11:44:22 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 // import './App.css';
 // import About from './components/About';
 
+import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
 import React, {useState} from 'react'
@@ -11,6 +12,16 @@ function App() {
    
   const[mode, setMode] = useState('light')
  
+const [alert, setAlert] = useState(null)
+
+
+const showAlert = (message,type)=>{
+  setAlert({
+  msg : message,
+  type : type
+})
+}
+
 const toggleMode = ()=>{
 
   if(mode==='light'){
@@ -18,11 +29,13 @@ const toggleMode = ()=>{
     setMode('dark')
 
     document.body.style.backgroundColor = '#123150'
+    showAlert("Dark mode enabled", "success")
   }
   else{
     setMode('light')
     
     document.body.style.backgroundColor = 'white'
+    showAlert("Light mode enabled", "success")
   }
 }
 
@@ -32,6 +45,7 @@ const toggleMode = ()=>{
 
 
 <Navbar title = "Test-Mod" about = "Details " mode ={mode} toggleMode={toggleMode}/>  
+<Alert alert = {alert}> </Alert>
 
 <div className='container my-3' >
 
